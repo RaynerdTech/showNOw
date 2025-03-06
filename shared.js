2e10f1371c32b0cfc85e5161cc99767c8f68796e
RaynerdTech <raynerdtech@gmail.com>
2025-03-06 20:58:05 +0100

    add footer script

--- shared.js
+++ shared.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sharedLayout = document.getElementById('sharedLayout');
+    const footershare = document.getElementById('footershare');
 
     // Fetch the shared layout HTML file
     fetch('/sharedfile.html')
@@ -8,8 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
             sharedLayout.innerHTML = html;
 
             // Dispatch a custom event once the shared layout is loaded
-            const event = new Event('sharedLayoutLoaded');
-            document.dispatchEvent(event);
+            document.dispatchEvent(new Event('sharedLayoutLoaded'));
         })
         .catch(err => console.error('Error loading shared content:', err));
+
+    // Fetch footershare.html
+    fetch('/sharedFooter.html')
+        .then(response => response.text())
+        .then(html => {
+            footershare.innerHTML = html;
+
+            // Dispatch a custom event once the footer share is loaded
+            document.dispatchEvent(new Event('footershareLoaded'));
+        })
+        .catch(err => console.error('Error loading footer share:', err));
+
+
 });
